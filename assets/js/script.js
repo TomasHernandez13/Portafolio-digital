84a7dd43349851705f1a8816bb0f5a034fd2a170
tomas <tomashmz@yahoo.com>
2024-07-21 21:30:17 -0400

    Nuevas secciones añadidas e interactividad por js añadida.

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function showForm() {
+    document.querySelector(".contact-form").style.display="block"
+    document.querySelector("#show-btn").remove()
+}
+
+function sendAlert() {
+    alert("Mensaje enviado")
+}
